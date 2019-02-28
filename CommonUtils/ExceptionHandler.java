d1a13f4be7cb52e5a58670faa3fdb3d4b3a6b6a6
liuyang <1227113413@qq.com>
2019-02-28 21:16:05 +0800

    提交测试111

---
+++ CommonUtils/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.ly.SpringBoot.CommonUtils;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 对统一的异常进行捕获等待
+ */
+@ControllerAdvice
+public class ExceptionHandler {
+}
